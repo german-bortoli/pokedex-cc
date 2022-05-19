71c38f2baff49c1b3812f422d005bd9207aac9aa
German Bortoli <germanazo@gmail.com>
2022-05-19 19:41:06 -0300

    Added per page

--- src/pages/PokemonListPage.tsx
+++ src/pages/PokemonListPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 
+import { FormControl, InputLabel, NativeSelect } from '@mui/material';
 import { Container } from '@mui/system';
 
 import PokemonsList from '../components/PokemonsList';
@@ -7,8 +8,10 @@ import ThemePagination from '../components/ThemePagination';
 import { usePokemonList } from '../hooks/usePokemonList';
 import { LimitOffsetType } from '../types';
 
+const DEFAULT_LIMIT = 20;
+
 const PokemonListPage = () => {
-  const limit = 16;
+  const [limit, setLimit] = useState(DEFAULT_LIMIT);
   const [offset, setOffset] = useState(0);
   const { data, isLoading } = usePokemonList(offset, limit);
 
@@ -18,6 +21,28 @@ const PokemonListPage = () => {
 
   return (
     <Container>
+      <Container>
+        <FormControl>
+          <InputLabel variant="standard" htmlFor="uncontrolled-native">
+            Per Page
+          </InputLabel>
+          <NativeSelect
+            defaultValue={DEFAULT_LIMIT}
+            onChange={e => setLimit(Number(e.target.value))}
+            inputProps={{
+              name: 'perPage',
+              id: 'uncontrolled-native',
+            }}
+          >
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+            <option value={30}>30</option>
+            <option value={40}>40</option>
+            <option value={50}>50</option>
+            <option value={100}>100</option>
+          </NativeSelect>
+        </FormControl>
+      </Container>
       <Container>
         <PokemonsList pokemons={data?.pokemons || []} isLoading={isLoading} />
         <ThemePagination
