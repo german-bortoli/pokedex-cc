2fe326a6d89a49aaae9bc75d0d124f2fc5e54ebb
German Bortoli <germanazo@gmail.com>
2022-05-23 09:42:09 -0300

    Revert prettier error on stories

--- src/stories/PokemonProfile.stories.tsx
+++ src/stories/PokemonProfile.stories.tsx
@@ -7,28 +7,30 @@ import { pokemon } from '../fixtures/pokemonProfile';
 
 export default {
   title: 'Components/Pokemons/Profile',
-  component: PokemonProfile
+  component: PokemonProfile,
 } as ComponentMeta<typeof PokemonProfile>;
 
-const Template: ComponentStory<typeof PokemonProfile> = args => (<PokemonProfile {...args} />);
+const Template: ComponentStory<typeof PokemonProfile> = args => (
+  <PokemonProfile {...args} />
+);
 
 export const Profile = Template.bind({});
 Profile.args = {
   pokemon,
   isLoading: false,
-  isError: false
+  isError: false,
 };
 
 export const Loading = Template.bind({});
 Loading.args = {
   pokemon: undefined,
   isLoading: true,
-  isError: false
+  isError: false,
 };
 
 export const Error = Template.bind({});
 Error.args = {
   pokemon: undefined,
   isLoading: true,
-  isError: true
+  isError: true,
 };
